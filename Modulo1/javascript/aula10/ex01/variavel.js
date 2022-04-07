bd3b23afa7b2fb057e8e7cc595675c024767997d
Polly Santos <99842806+PollySantos@users.noreply.github.com>
2022-04-07 03:30:30 -0300

    commit

--- Modulo1/javascript/aula10/ex01/variavel.js
+++ Modulo1/javascript/aula10/ex01/variavel.js
@@ -1,15 +1,3 @@
-var x = "Poliana"
-var y = 29
-
-console.log( 'O usuário de nome ' + x + ' tem ' +  y + ' anos.')
-
-var x = "Flávio"
-var y = 25
-
-console.log( 'O usuário de nome ' + x + ' tem ' +  y + ' anos.')
-
-// ou
-
 var nome = "Poliana"
 var idade = 29
 
--- Modulo1/javascript/aula10/var_typeof.js
+++ Modulo1/javascript/aula10/var_typeof.js
@@ -12,9 +12,9 @@ console.log('Total: ' + saldo + deposito)
 
 console.log('Novo depósito na conta ' + conta)
 console.log('Saldo utilizado: ' + saldo)
-console.log(typeof(cliente))
-console.log(typeof(conta))
-console.log(typeof(saldo))
-console.log(typeof(deposito))
+console.log('tipo: ' + typeof(cliente))
+console.log('tipo: ' + typeof(conta))
+console.log('tipo: ' + typeof(saldo))
+console.log('tipo: ' + typeof(deposito))
 
 //ao somar uma string com um number, automaticamente já converte tudo para texto, por isso que resulta em uma concatenação.
