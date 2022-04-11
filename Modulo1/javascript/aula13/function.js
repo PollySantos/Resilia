3e18086cfc922edb5be2c7beee38b73e1b66b2f3
Polly Santos <99842806+PollySantos@users.noreply.github.com>
2022-04-11 20:53:42 -0300

    Learning JS

---
+++ Modulo1/javascript/aula13/function.js
@@ -0,0 +1,11 @@
+function dirigirEBeber (idade) {
+    if (idade > 18) {
+        return  'Você pode dirigir e beber!'
+    } else {
+        return 'Você não pode dirigir nem beber: '
+    }
+}
+
+function previsaoRodagem (liros, quilometragem) {
+    return 'O carro pode rodar' + quilometragem/litros + 'km.';
+}
