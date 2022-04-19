1cc0c0c1a0f72446ad0f8dbbbb9be2824da06c65
Polly Santos <99842806+PollySantos@users.noreply.github.com>
2022-04-18 21:05:30 -0300

    js

---
+++ Modulo1/javascript/aula17/script.js
@@ -0,0 +1,14 @@
+var nome = (prompt('Digite um numero'));
+
+function linhaAsterisco(n) {
+    var asterisco = ""
+    for (var i=0; i < n; i++) {
+       asterisco = "";
+    for (var p = 0; p <= i; p++) {
+        asterisco += "*"
+    }
+        console.log(linhaAsterisco)
+    }
+}
+
+linhaAsterisco(nome);
