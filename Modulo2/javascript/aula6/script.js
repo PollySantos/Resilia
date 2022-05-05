bffce957f391ad8e44607f8d3651a472fa6ed2d4
Polly Santos <99842806+PollySantos@users.noreply.github.com>
2022-05-05 14:25:29 -0300

    API js

---
+++ Modulo2/javascript/aula6/script.js
@@ -0,0 +1,37 @@
+function geraUsuario() {
+    var req = new XMLHttpRequest();
+    req.open('GET', 'https://randomuser.me/api');
+    req.onload = function() {
+        if (req.status == 200) {
+            usuarioTxt = req.response;
+            usuarioObj = JSON.parse(usuarioTxt);
+            addUsrPag(usuarioObj);
+        } else {
+            console.log('deu algum erro de status' + req.status);
+        }
+    };
+    
+    req.send();
+}
+
+function addUsrPag(respObj) {
+    var usr = respObj.results[0];
+    var nameNode = genNodeName(usr);
+    var emailNode = genNodeEmail(usr);
+    var usrDiv = document.querySelector(".usr");
+    usrDiv.innerHTML = "";
+    usrDiv.appendChild(nameNode);
+    usrDiv.appendChild(emailNode);
+  }
+  
+  function genNodeName(usr) {
+    var usrName = document.createElement("h2");
+    usrName.textContent = `${usr.name.first} ${usr.name.last}`;
+    return usrName;
+  }
+  
+  function genNodeEmail(usr) {
+      var usrEmail = document.createElement("h4");
+    usrEmail.textContent = `${usr.email}`;
+    return usrEmail;
+  }
